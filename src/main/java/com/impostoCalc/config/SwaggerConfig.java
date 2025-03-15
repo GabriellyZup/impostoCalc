44469649ee1c47ac1cf1af54db67109f817579d4
GabriellyZup <gabrielly.boareto@zup.com.br>
2025-03-15 20:22:51 -0300

    feat: add TaxType entity

--- src/main/java/com/impostoCalc/config/SwaggerConfig.java
+++ src/main/java/com/impostoCalc/config/SwaggerConfig.java
@@ -1,4 +1,24 @@
 package com.impostoCalc.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.info.License;
+
+@Configuration
+
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Tax Calculation API")
+                        .version("1.0")
+                        .description("API para cálculo de impostos")
+                        .license(new License().name("Apache 2.0").url("https://springdoc.org")));
+    }
 }
--- src/main/java/com/impostoCalc/dtos/TaxTypeRequestDTO.java
+++ src/main/java/com/impostoCalc/dtos/TaxTypeRequestDTO.java
@@ -1,4 +1,42 @@
 package com.impostoCalc.dtos;
 
-public class RequestDTO {
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.math.BigDecimal;
+
+@Schema(description = "Objeto de requisição para criar ou atualizar um tipo de imposto.")
+
+public class TaxTypeRequestDTO {
+
+    @Schema(description = "Nome do tipo de imposto.", example = "ICMS", required = true)
+    private String nome;
+
+    @Schema(description = "Descrição detalhada do tipo de imposto.", example = "Imposto sobre Circulação de Mercadorias e Serviços")
+    private String descricao;
+
+    @Schema(description = "Alíquota do imposto.", example = "18.00", required = true)
+    private BigDecimal aliquota;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getAliquota() {
+        return aliquota;
+    }
+
+    public void setAliquota(BigDecimal aliquota) {
+        this.aliquota = aliquota;
+    }
 }
