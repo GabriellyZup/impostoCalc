c90ecdb94f738d0bb47f5752e2104fc1058b488d
GabriellyZup <gabrielly.boareto@zup.com.br>
2025-03-15 18:03:53 -0300

    first commit

---
+++ src/main/java/com/impostoCalc/ImpostoCalcApplication.java
@@ -0,0 +1,13 @@
+package com.impostoCalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpostoCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpostoCalcApplication.class, args);
+	}
+
+}
